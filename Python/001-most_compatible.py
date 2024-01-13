332bd473ae3818d3625b7c9dee48d01eda506a08
Antonio-III <59204403+Antonio-III@users.noreply.github.com>
2024-01-13 13:12:43 +0400

    Update 001-most_compatible.py

--- Python/001-most_compatible.py
+++ Python/001-most_compatible.py
@@ -14,6 +14,7 @@ def simulate(amount=1):
     global people_group, comparison_group, comparison_list, chosen, chosen_list, chosen_max_count
     
     for r in range(amount):
+        # Simulation count
         print(f"Simulation: {r+1}")
 
         # Change the order for every simulation
@@ -35,31 +36,35 @@ def simulate(amount=1):
                 # Count how many times we encountered the highest rated person among all simulations
                 if date==100:
                     chosen_max_count+=1
-                
+
+                # Person will be our chosen
                 chosen=date
                 chosen_list.append(chosen)
-                print(f"Highest in comparison: {max(comparison_list)}\n")
+                
                 break
             
             # If person isn't higher than the highest in comparison group
             if date < max(comparison_list):
                 continue
+        
+        print(f"Highest in comparison: {max(comparison_list)}\n")
 
-        # Clear the list for the next simulation
+        # Clear the list for the next simulation,
+        # so as not to include ratings that shouldn't have been there
         comparison_list.clear()
         
-        
+    # Print details at the end of simulation
     print(f"Chosen Group: {chosen_list}")
     print(f"Times 100 is chosen: {chosen_max_count}")
     print(f"Median: {statistics.median(chosen_list)}")
     print(f"Average of chosen group: {sum(chosen_list)/len(chosen_list)}")
 
 
-# Execution the simulation
+# Execution of the simulation
 simulate(
         100
         # Replace the number with any number, bigger numbers will take longer to finish, 
-        # So maybe cap it to 10,000
+        # so maybe cap it to 10,000
         )
 
     
