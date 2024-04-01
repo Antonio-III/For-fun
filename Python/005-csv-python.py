3c728d02a6416bedf150e03ab23f4054cff43941
Antonio-III <59204403+Antonio-III@users.noreply.github.com>
2024-04-01 14:11:56 +0400

    update averages.csv

--- Python/005-csv-python.py
+++ Python/005-csv-python.py
@@ -1,33 +1,49 @@
 import csv
-from io import TextIOWrapper
 
-# step 1 (optional): create a filtered file 
-def create_mod_file( input_csv_file_dir: str, name_new_file: str, include_columns: list, columns_where_the_conditions_are: list = [], conditions_must_be: list = [] ):
+#Primary function
+# Algorithm of `create_mod_file`: 
+    # 0.    (1) Input directory and or file name of a csv. 
+    #       (2) Input name of the created modified file. 
+    #       (3) Input column numbers of included columns in a list object. 
+    #       (4) Input column numbers who contain conditions you want to check (optional). Must be in a list object. 
+    #       (5) Input `str` of what those conditions should be. Must be in a list object.
+    # 1. Offset by 1 to have zero-based index.
+    # 1. Open inputted file.
+    # 2. Make `writer` object of said file, so we can access `writerow` method for later.
+    # 3. Iterate through a returned object from `filtered_rows`. `entry` is the reference.
+    # 4. Make a generator object consisting of the current iteration's elements, but only including the column numbers that the user included. 
+    # 5. Pass this generator object to `writerow` method of the `writer` object, essentially wiriting new text to the new `.csv` file.
+    # 6. Return `writer` object (optional), if you need it for something else. 
 
-    
+def create_mod_file( input_csv_file_dir: str, name_new_file: str, include_columns: list, col_num_of_conds: list = [], conds_must_be: list = [], 
+                    skip_first_line: bool = True ):
+
+    incl_col_offset = [ (i - 1) for i in include_columns ] # Hard coded 
 
     with open( name_new_file, "w", newline="" ) as mod:
         created_file = csv.writer( mod )
-
-        for entry in filtered_rows( input_csv_file_dir, columns_where_the_conditions_are, conditions_must_be ):
-            created_file.writerow(  [ entry[i] for i in include_columns ] )
+        for entry in filtered_rows( input_csv_file_dir, col_num_of_conds, conds_must_be, skip_first_line ):
+            created_file.writerow(  [ entry[i] for i in incl_col_offset ] )
+            
 
     return created_file # optional
 
-# Algorithm of `create_mod_file`:
-    # 0. Input directory and or file name of a csv. Input name of the created modified file. Input included columns. Input column numbers (int) who contain conditions you want to check (optional). Must be in a list object. Input `str` of what those conditions should be. Must be in a list object.`
-    # 1. Open file
-    # 2. Make `writer` object of said file, so we can access `writerow` method for later.
-    # 3. Iterate through a returned object from `filtered_rows`
-    # 4. Make generator object consisting of the current iteration's (named `entry`) elements. 
-    # 5. Pass this generator object to `writerow` method of the `writer` object, essentially creating 
 
-def get_avg( input_csv_file_dir: str, column_to_get_avg_of: int, columns_where_the_conditions_are: list = [], conditions_must_be: list = [], rounded_to: int = 0  ):
+
+# Algorithm of `get_avg`
+    # 0.    (1) Input csv file name and or directory. Input column number to get the average of. Column number must be numeric only. 
+    #       (2) Input column numbers of conditions (optional), in a list object. 
+    #       (3) Input what the respective conditions should be in a list object (optional). Both these arguments, while optional, MUST be the same length. 
+    #       (4) Input integer to be rounded (defaults to nearest `int`, optional).
+    # 1. Create reference to returned list object from `filtered_rows`
+    # 2. 
+
+def get_avg( input_csv_file_dir: str, col_num_to_get_avg_of: int, col_num_of_conds: list = [], conds_must_be: list = [], rounded_to: int = 0  ):
     
-    rows = filtered_rows( input_csv_file_dir, columns_where_the_conditions_are, conditions_must_be )
+    rows = filtered_rows( input_csv_file_dir, col_num_of_conds, conds_must_be )
 
     target = []
-    column_index = column_to_get_avg_of - 1
+    column_index = col_num_to_get_avg_of - 1 
     
     for element in rows:
         target.append( element[column_index] ) # element[column_index] MUST be an int
@@ -43,10 +59,11 @@ def get_avg( input_csv_file_dir: str, column_to_get_avg_of: int, columns_where_t
 # If you want to have non-equal comparisons, use `greater_than`, `less_than`, `not`, separated by `-`. 
 # Ex: filtered_rows("test.csv", "4", "less_than-0").
     # This returns rows with values that are `condition <0` in column `4`. 
-def filtered_rows( input_csv_file_dir: str, columns_where_the_conditions_are: list = [], conditions_must_be: list = [], skip_first_line: bool = True ):
-    result = []
-
+# This is for stripping unwanted columns, or just get the row's values.
+def filtered_rows( input_csv_file_dir: str, col_num_of_conds: list = [], conds_must_be: list = [], skip_first_line: bool = True ):
     special_conditions = {"not": "!=", "greater_than": ">", "less_than": "<"}
+
+    result = []
 
     with open( input_csv_file_dir, "r" ) as opened_file:
         opened_file = csv.reader(opened_file)
@@ -59,18 +76,18 @@ def filtered_rows( input_csv_file_dir: str, columns_where_the_conditions_are: li
             conditions_met = True
             
             
-            for column, condition in zip( columns_where_the_conditions_are, conditions_must_be ):
+            for column, condition in zip( col_num_of_conds, conds_must_be ):
                 column_index = column - 1 # Users will input the column number starting from 1. We offset by 1. Hard coded
 
                 splitted_condition = condition.lower().split( "-" ) # Will look like: ["not", "abc"] or ["abc"] when passed ["not-abc"] or ["abc"]
-                splitted_condition[ len(splitted_condition)-1 ] = [ ord(i) for i in splitted_condition[-1] ] # Will be like [123]. "abc" became `[int("123")]`. Note that it became a list as well. It is also hard coded in a way the last element ought to be the value we're looking for
+                splitted_condition[ len(splitted_condition)-1 ] = [ ord(i) for i in splitted_condition[-1] ] # "abc" will be like [1,2,3]. Note that it became a list as well. It is also hard coded in a way the last element ought to be the value we're looking for
 
-                # special_condition will be: [ "not", [123] ] or [ 123 ]
+                # special_condition at this point will be: [ "not", [1,2,3] ] or [ 1,2,3 ]
 
-                # reassigned `condition` from `line 55`
+                # `condition` from `for` loop reassigned
                 special_word, condition = splitted_condition if len( splitted_condition ) >= 2 else ( None, splitted_condition[-1] )
 
-                val_in_col_nums = [ ord(i) for i in row[column_index].lower() ]
+                val_in_col_nums = [ ord(i) for i in row[column_index].lower() ] # "abc" will be like [1,2,3]
 
                 try:
                     if eval(f"""{val_in_col_nums} {special_conditions.get(special_word, "==")} {condition}"""):
@@ -106,7 +123,7 @@ def get_unique_values( input_csv_file_dir: str, target_column: int,  ):
     return sorted( set(names) )
 
 # step 2 (the only good thing in this script): write a csv file with the calculated column_4s 
-def create_avg_csv(input_csv_file_dir: str, file_name: str, header: list, column_to_get_avg_of: int, get_uniq_val_in_this_cols: list,  ):
+def create_avg_csv(input_csv_file_dir: str, file_name: str, header: list, col_num_to_get_avg_of: int, get_uniq_val_in_this_cols: list,  ):
     with open( file_name, "w", newline="" ) as mod:
         created_file = csv.writer( mod )
 
@@ -117,5 +134,29 @@ def create_avg_csv(input_csv_file_dir: str, file_name: str, header: list, column
 def create_nested_loops( starting_index: int = 0, starting_list: list = [] ):
     # write your function nigga
     pass
-    
-print(filtered_rows("mod_ds_salaries.csv", [2,3],["MI","Data Scientist"]))
+
+
+# Primary Function
+def round_values(input_csv_file_dir: str, output_csv_file: str, target_col_num: int, skip_first_line: bool = True, rounded_to: int = 0, header: list = []):
+    col_index = target_col_num - 1 # Offset. Hard coded
+
+
+    with open(output_csv_file, "w", newline="") as file_write:
+        file_write = csv.writer(file_write)
+        
+        file_write.writerow(header) if header else None
+
+        for entry in filtered_rows(input_csv_file_dir, skip_first_line = skip_first_line):
+          
+            file_write.writerow( [ entry[i] for i in range(col_index) ] + [ round( float( entry[col_index] ), rounded_to ) ] )
+
+
+def colums_to_rows(input_csv_file_dir: str, output_csv_file: str):
+
+    with open(output_csv_file, "w") as file_writer:
+        file_writer = csv.writer( file_writer )
+
+        for entry in filtered_rows(input_csv_file_dir):
+            file_writer.writerow( entry[] )
+round_values("no-0-avg.csv", "rounded.csv", 4, rounded_to= 0, header =["work_year", "experience_level", "job_title", "salary_in_usd"] )
+#print(filtered_rows("no-0-avg.csv"))
