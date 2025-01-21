41fb717f0bc0a70632afef584fbb909d3a3d511a
Antonio-III <59204403+Antonio-III@users.noreply.github.com>
2025-01-21 15:22:10 +0400

    Update 021-rename-files.py
    simplified the code

--- Python/021-rename-files.py
+++ Python/021-rename-files.py
@@ -3,15 +3,14 @@ import os
 def main(directory:str)->None:
     rename_files(directory)
     print("Done!\n")
+
     return None
 
 def rename_files(directory:str)->None:
     """
-    Made specificially for renaming .zip files from this format: Author - Album Title (Year), to: Year - Album Title.
+    Renames .zip files from this format: 'Author - Album Title (Year)' to: 'Year - Album Title'.
     """
     files=os.listdir(directory)
-    
-
     for old_folder in files:
         # _ is the author name, which we don't need.
         _,album_name=old_folder.strip(".zip").split("-",1)
@@ -25,14 +24,14 @@ def rename_files(directory:str)->None:
             year=suffix
             new_folder=f"{year} - {" ".join(album_name)}.zip"
 
-            new_path=os.path.join(directory,new_folder)
-            old_path=os.path.join(directory,old_folder)
-            
+            new_path=fr"{directory}\{new_folder}"
+            old_path=fr"{directory}\{old_folder}"
+
             os.rename(old_path,new_path)
 
-            print(f"Renamed '{old_folder}' to '{new_folder}'")
+            print(f"Renamed '{old_folder}' to '{new_folder}'.")
 
     return None
 if __name__=="__main__":
-    DIRECTORY=rf"{input("Enter directory:\n")}"
+    DIRECTORY=fr"{input("Enter directory:\n")}"
     main(DIRECTORY)
