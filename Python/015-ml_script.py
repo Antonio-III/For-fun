9e8762b2e71a8cb03f9378902078567c26926329
Antonio Intal <59204403+Antonio-III@users.noreply.github.com>
2025-05-04 20:48:03 +0400

    Update 015-ml_script.py

--- Python/015-ml_script.py
+++ Python/015-ml_script.py
@@ -11,7 +11,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 
 # You can add supported models here. Make sure they are imported
-SUPPORTED_MODELS= ["LinearRegression()"]
+SUPPORTED_MODELS= ["LinearRegression()", "KNeighborsClassifier()", "DecisionTreeClassifier()"]
 
 from sklearn.preprocessing import LabelEncoder
 
@@ -32,7 +32,8 @@ HEATMAP_SETTINGS = {"annot": True, "cmap": "Blues"}
 FIG_SIZE = (15,8)
 
 # Do not change
-BIGGEST_SEED = 2**32- 1
+MAX_SEED = 2**32- 1
+MIN_SEED = 0
 
 NO_SEED_INPUT= -1
 
@@ -41,6 +42,7 @@ FIRST_RESULT = "Classification Result"
 CLASSIFICATION_RESULT_TEMPLATE = "Accuracy score: {accuracy_sc}\nClassification Report: {classification_re}"
 R_SQ_TEMPLATE = "Mean Squared Error: {mean_sq_err}\nMean Absolute Error: {mean_abs_err}\nR Squared Score: {rsq_sc}"
 
+# Check functions
 def args_match_dtype(*args,dtype):
     try:
         for arg in args:
@@ -50,44 +52,48 @@ def args_match_dtype(*args,dtype):
     else:
         return True
 
-# Check functions
 def args_are_str(*args):
-    return args_match_dtype(args,dtype=str)
+    return args_match_dtype(args, dtype = str)
 def args_are_int(*args):
-    return args_match_dtype(args,dtype=int)
+    return args_match_dtype(args, dtype = int)
 def args_are_float(*args):
-    return args_match_dtype(args,dtype=float)
+    return args_match_dtype(args, dtype = float)
 def args_are_list(*args):
-    return args_match_dtype(args,dtype=list)
+    return args_match_dtype(args, dtype = list)
 def args_are_bool(*args):
-    return args_match_dtype(args,dtype=bool)    
+    return args_match_dtype(args, dtype = bool)    
 
 def all_inputs_valid(**kwargs) -> bool:    
-    check_1 = args_are_str(kwargs["dataset"],kwargs["dependent_var"])
+    """
+    Checks if inputted values meet the same data type in the model_predict function. Some data types should be converted to their respective data types. For instance, the value for test_size should be convertible to float data type.
+    """
+    check_1 = args_are_str(kwargs["dataset"], kwargs["dependent_var"])
     check_2 = args_are_int(kwargs["random_state"])
     check_3 = args_are_float(kwargs["test_size"])
     check_4 = args_are_list(kwargs["str_columns"])
     check_5 = args_are_bool(kwargs["str_data_present"])
 
-    checks = (check_1,check_2,check_3,check_4,check_5)
+    checks = (check_1, check_2, check_3, check_4, check_5)
+
     return all(checks)    
+# ---- End of check functions ----
 
 # Model function
-def model_predict(dataset:str,
-                  dependent_var:str, 
-                  str_data_present:bool,
-                  str_columns:list,
-                  random_state:int,
-                  test_size:float,
+def model_predict(dataset: str,
+                  dependent_var: str, 
+                  str_data_present: bool,
+                  str_columns: list,
+                  random_state: int,
+                  test_size: float,
                   model_used: str,
                  ) -> tuple:  
 
-    if all_inputs_valid(dataset             =dataset,
-                        dependent_var       =dependent_var, 
-                        str_data_present    =str_data_present,
-                        str_columns         =str_columns,
-                        random_state        =random_state,
-                        test_size           =test_size,
+    if all_inputs_valid(dataset = dataset,
+                        dependent_var = dependent_var, 
+                        str_data_present = str_data_present,
+                        str_columns = str_columns,
+                        random_state = random_state,
+                        test_size = test_size,
                         ):
         
         # 1.2 Loading dataset.
@@ -97,44 +103,44 @@ def model_predict(dataset:str,
         print("Dataset loading complete.")
 
         # Visualization
-        visualize_data(df=df) if not str_data_present else None
+        visualize_data(df = df) if not str_data_present else None
 
         # 2. Data cleaning/preprocessing.
-        X,y = data_preprocessing(df=df,dependent_var=dependent_var,str_data_present=str_data_present)
+        X,y = data_preprocessing(df = df, dependent_var = dependent_var, str_data_present = str_data_present)
 
         print(f"Features (X):\n{X.head()}")
         print(f"Features (y):\n{y.head()}")
         print("Data cleaning complete.")
 
         # 3. Data splitting
-        X_train, X_test, y_train, y_test = data_splitting(X,y,test_size=test_size,random_state=random_state)
+        X_train, X_test, y_train, y_test = data_splitting(X, y, test_size = test_size, random_state = random_state)
 
         print(f"Training set size: {X_train.shape[0]}.")
         print(f"Test set size:{X_test.shape[0]}.")
         print("Data splitting complete.")
 
         # 4. Model training.
-        model = model_training(model_used=model_used,X_train=X_train,y_train=y_train)
+        model = model_training(model_used = model_used, X_train = X_train, y_train = y_train)
 
         print("Model training complete.")
 
         # 5. Model testing.
 
-        y_pred = model_testing(model=model,X_test=X_test)
+        y_pred = model_testing(model = model, X_test = X_test)
         print("Model testing complete.")
 
         # 6. Model evaluation.
         
-        output = model_evalution(y_test=y_test,y_pred=y_pred)
+        output = model_evalution(y_test = y_test, y_pred = y_pred)
         return output
     
-def load_dataset(dataset:str) -> pd.DataFrame:
+def load_dataset(dataset: str) -> pd.DataFrame:
     """
     Step 1 of the Machine Learning steps. This functions returns a DataFrame object, which can be used in the next step.
     """
     return pd.read_csv(dataset)
 
-def visualize_data(df:pd.DataFrame) -> None:
+def visualize_data(df: pd.DataFrame) -> None:
     """
     Visualizes data. sns.heatmap, pd.DataFrame.hist, and plt.Figure methods are used.
     """
@@ -150,7 +156,7 @@ def visualize_data(df:pd.DataFrame) -> None:
 
     return None
 
-def data_preprocessing(df:pd.DataFrame,dependent_var:str,str_data_present:bool) -> tuple[pd.DataFrame, pd.Series]:
+def data_preprocessing(df: pd.DataFrame, dependent_var: str, str_data_present: bool) -> tuple[pd.DataFrame, pd.Series]:
     """
     Step 2 of the Machine Learning steps. Converts textual columns to numerical data, and returns 2 objects: The independent variables (X) and the dependent variable (y).
     """
@@ -171,11 +177,14 @@ def convert_text_col_to_num(df:pd.DataFrame) -> pd.DataFrame:
 
     return df
 
-def data_splitting(X:pd.DataFrame,y:pd.Series,test_size:float,random_state:int) -> tuple[pd.DataFrame, pd.Series, pd.DataFrame, pd.Series]:
+def data_splitting(X: pd.DataFrame, y: pd.Series, test_size: float, random_state: int) -> tuple[pd.DataFrame, pd.Series, pd.DataFrame, pd.Series]:
     """
     Step 3 of the Machine Learning steps. Splits the independent and dependent variables into 2 categories: Training, and Testing.
     """
-    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=test_size,random_state=random_state)
+    X_train, X_test, y_train, y_test = train_test_split(X, 
+                                                        y, 
+                                                        test_size = test_size, 
+                                                        random_state = random_state)
 
     return X_train, X_test, y_train, y_test
 
@@ -186,10 +195,10 @@ def model_training(model_used: str, X_train: pd.DataFrame, y_train: pd.Series):
     """
     model = model_used
 
-    model.fit(X_train,y_train)
+    model.fit(X_train, y_train)
     return model
 
-def model_testing(model,X_test: pd.DataFrame) -> np.ndarray:
+def model_testing(model, X_test: pd.DataFrame) -> np.ndarray:
     """
     Step 5 of the Machine Learning steps. Tests the model with the given training dataset. Returns the prediction.
     """
@@ -206,8 +215,8 @@ def model_evalution(y_test: pd.Series, y_pred: pd.Series) -> tuple:
         try:
             if result == FIRST_RESULT:
                 results[result] = CLASSIFICATION_RESULT_TEMPLATE.format(
-                    accuracy_sc = accuracy_score(y_test,y_pred), 
-                    classification_re = classification_report(y_test,y_pred)
+                    accuracy_sc = accuracy_score(y_test, y_pred), 
+                    classification_re = classification_report(y_test, y_pred)
                     )
             else: # Regression
                 results[result] = R_SQ_TEMPLATE.format(
@@ -220,6 +229,28 @@ def model_evalution(y_test: pd.Series, y_pred: pd.Series) -> tuple:
     
     return tuple(results.values())  
 
+def set_random_state(seed: str) -> int:
+    """
+    Returns an int object to be used as the value for the random_state variable. The function checks if the inputted seed is a number, and must be at least 0. 
+    """
+    try:
+        seed = int(seed)
+    except TypeError:
+        random_state = generate_random_seed()
+    else:
+        random_state = seed if seed >= MIN_SEED and seed <= MAX_SEED else generate_random_seed(ommit_error = True)
+    finally:
+        return random_state 
+
+def generate_random_seed(ommit_error: bool = False) -> int:
+    """
+    In the case of errors in setting the seed, a random seed is returned instead.
+    """
+    if not ommit_error:
+        print(f"Entered seed does not meet numbers in the range({MIN_SEED}~{MAX_SEED}). Using random seed instead.")
+
+    random_state = random.randrange(MAX_SEED)
+    return random_state
 
 if __name__ == "__main__":
     dataset = input("Enter dataset path including the dataset file itself. Path must be in your computer directory:\n").strip('"').strip("'")
@@ -233,8 +264,9 @@ if __name__ == "__main__":
     if str_data_present:
         eval(str_columns)
 
-    seed = int(input(f"Enter seed (Enter {NO_SEED_INPUT} to auto generate seed):\n"))
-    random_state = seed if seed!=NO_SEED_INPUT else random.randrange(BIGGEST_SEED)
+    seed = input(f"Enter seed (Enter {NO_SEED_INPUT} to auto generate seed). Must be at least {MIN_SEED}:\n")
+
+    random_state = set_random_state(seed)
 
     test_size = int(input("Enter the test/train split for test size (E.g. 70 if 70/30):\n"))/100
     
@@ -243,7 +275,11 @@ if __name__ == "__main__":
         print(f"Model used: {model}.")
         model_used = eval(model)
 
-        output = model_predict(dataset=dataset,dependent_var=dependent_var,str_data_present=str_data_present,str_columns=str_columns,random_state=random_state,test_size=test_size,model_used=model_used)
+        output = model_predict(dataset = dataset,
+                               dependent_var = dependent_var,str_data_present = str_data_present,str_columns = str_columns,
+                               random_state = random_state,
+                               test_size = test_size,
+                               model_used = model_used)
 
         for result in output:
             print(result)
